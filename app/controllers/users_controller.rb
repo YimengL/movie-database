01ba1abe82bdca910a08e49bd608c5286fb8198f
YimengLi <lym1108csu@gmail.com>
2016-04-26 21:22:46 +0000

    FINISH log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Movie Database!"
       redirect_to user_url(@user)
     else
